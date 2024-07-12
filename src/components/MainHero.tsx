fd1bfb924a245694b82a8150eeabbd8ab52225ab
SuhanaJabin <suhanajabinpk@gmail.com>
2024-07-13 03:36:38 +0530

    fixing orinetation 2

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -20,11 +20,11 @@ const MainHero = () => {
         {/* <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           {mainHero.description}
         </p> */}
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:ml-28 mb-16 lg:justify-start">
+        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:ml-28 mb-16 ">
           <div className="rounded-md shadow">
             <a
               href={mainHero.primaryAction.href}
-              className={`w-full flex items-center  lg:mt-10 sm:mt-10 justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className={`lg:w-full sm:w-1/2 flex items-center  lg:mt-10 sm:mt-10  px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.primaryAction.text}
             </a>
@@ -32,7 +32,7 @@ const MainHero = () => {
           <div className="  sm:ml-3">
             <a
               href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center text-center lg:mt-10 sm:mt-10 justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className={`lg:w-full sm:w-1/2 flex items-center text-center lg:mt-10 sm:mt-10 justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.secondaryAction.text}
             </a>
