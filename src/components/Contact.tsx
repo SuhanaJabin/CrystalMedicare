43492998647925ede07b166cc7c2aac48bbfaf23
SuhanaJabin <suhanajabinpk@gmail.com>
2024-07-13 06:55:15 +0530

    padding

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div id="about" className="mx-auto container xl:px-20 lg:px-12 py-12">
       <div className="flex items-center justify-center">
-        <div className="flex w-full p-1 lg:w-5/6 flex-wrap items-center mb-10 justify-center">
+        <div className="flex w-full lg:w-5/6 flex-wrap items-center mb-10 justify-center">
           <div>
             <h1 className="lg:text-5xl text-3xl text-gray-900 text-center font-bold mb-10">
               CONTACT 
@@ -14,26 +14,26 @@ const Contact = () => {
             </p>
 
             <div className="flex flex-row mt-5">
-              <p className="pl-1 text-xl font-semibold lg:text-2xl text-gray-500 text-justify">
+              <p className="text-xl font-semibold lg:text-2xl text-gray-500 text-justify">
                 Address:
               </p>
-              <div className="flex flex-col pl-1 font-semibold ml-5 text-xl lg:text-2xl text-gray-500 text-justify">
+              <div className="flex flex-col ml-5 text-xl font-semibold lg:text-2xl text-gray-500 text-justify">
                 <p>Apartment 38</p>
                 <p>Hill House Commercial Gate,</p>
                 <p>Mansfield,</p>
                 <p>NG18 1EJ</p>
               </div>
             </div>
-            <h1 className="mt-2 pl-1 text-xl lg:text-2xl text-gray-500 font-semibold text-justify">
+            <h1 className="mt-2 text-xl lg:text-2xl text-gray-500 font-semibold text-justify">
               Contact No: +447438035802
             </h1>
-            <h1 className="mt-2 pl-1 text-xl lg:text-2xl text-gray-500 font-semibold text-justify">
+            <h1 className="mt-2 text-xl lg:text-2xl text-gray-500 font-semibold text-justify">
               Email:{' '}
               <a className="text-gray-500 font-semibold transition-all duration-300" href="mailto:crystalmedicare.co.uk@gmail.com">
                 crystalmedicare.co.uk@gmail.com
               </a>
             </h1>
-            <p className="transition-all duration-500 pl-1 font-semibold hover:text-primary mt-5 text-xl lg:text-2xl text-gray-500 p-3 text-justify">
+            <p className="transition-all duration-500 hover:text-primary mt-5 text-xl lg:text-2xl text-gray-500 font-semibold p-3 text-justify">
               For appointments or enquiries, feel free to call, email or visit our website. Our dedicated team is ready to help you achieve optimal health and wellbeing.
             </p>
           </div>
